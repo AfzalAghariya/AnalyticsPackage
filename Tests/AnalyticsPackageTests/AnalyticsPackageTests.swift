39f5da7bd37e4fc09f48b0434c734808b85646e5
Afzal <vntech-engineering115@viralnation.com>
2023-10-05 16:58:42 +0530

    Initial Commit

---
+++ Tests/AnalyticsPackageTests/AnalyticsPackageTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import AnalyticsPackage
+
+final class AnalyticsPackageTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(AnalyticsPackage().text, "Hello, World!")
+    }
+}
